0637f544bb2b39592544e78fc38b053d697ac8c4
Abhinav Neelam <abhinavneelam949@gmail.com>
2019-05-04 20:15:39 -0700

    Testing new gui project

--- premake5.lua
+++ premake5.lua
@@ -4,12 +4,12 @@ workspace("Kengine")
 	platforms{"x64"}
 
 project("Kengine")
-   kind("ConsoleApp")
+   kind("StaticLib")
    language ("C++")
    targetdir ("%{prj.name}/bin/%{cfg.buildcfg}")
    objdir ("%{prj.name}/binobj/%{cfg.buildcfg}")
 
-   files { "**.h", "**.cpp" }
+   files { "%{prj.name}/**.h", "%{prj.name}/**.cpp" }
 
    filter("configurations:Debug")
       defines { "DEBUG" }
@@ -26,3 +26,29 @@ project("Kengine")
 	
 	filter("system:windows")
 		systemversion("latest")
+		filter ({})
+				
+project("Gui")
+   kind("ConsoleApp")
+   language ("C++")
+   targetdir ("%{prj.name}/bin/%{cfg.buildcfg}")
+   objdir ("%{prj.name}/binobj/%{cfg.buildcfg}")
+
+   files { "%{prj.name}/**.h", "%{prj.name}/**.cpp" }
+
+   filter("configurations:Debug")
+      defines { "DEBUG" }
+      symbols("On")
+
+   filter("configurations:Release")
+      defines { "RELEASE" }
+      optimize ("On")
+	  
+	filter ("platforms:x64")
+	  architecture ("x64")
+	  
+	includedirs { "%{prj.name}/include" }
+	
+	filter("system:windows")
+		systemversion("latest")
+		
