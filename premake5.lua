5822359ec883b686f0d6b9a9696345a64347cae2
Abhinav Neelam <abhinavneelam949@gmail.com>
2019-05-08 16:34:51 -0700

    Added qt for gui

--- premake5.lua
+++ premake5.lua
@@ -1,57 +1,100 @@
 --premake5 script
+require ("qt/qt")
+
+local qt = premake.extensions.qt
+
+local compilerversion = "msvc2017_64"
+--set this to the folder name that contains the include and lib folders in the qt directory
+
 workspace("Kengine")
 	configurations{"Debug","Release"}
 	platforms{"x64"}
+	startproject ("Gui")
 
 project("Kengine")
-   kind("ConsoleApp")
+   kind("StaticLib")
    location("Kengine")
    language ("C++")
+   cppdialect ("C++17")
+   staticruntime ("on")
+   
    targetdir ("%{prj.name}/bin/%{cfg.buildcfg}")
    objdir ("%{prj.name}/binobj/%{cfg.buildcfg}")
 
    files { "%{prj.name}/**.h", "%{prj.name}/**.cpp" }
+   includedirs { "%{prj.name}/include"}
 
    filter("configurations:Debug")
       defines { "DEBUG" }
+	  runtime("Debug")
       symbols("On")
 
    filter("configurations:Release")
+	  runtime("Release")
       defines { "RELEASE" }
       optimize ("On")
 	
 	filter ("platforms:x64")
 	  architecture ("x64")
-	  
-	includedirs { "%{prj.name}/include" }
-	
+	  	
 	filter("system:windows")
 		systemversion("latest")
 		
 		filter ({})
 
-project("Gui")
+project ("Gui")
    kind("ConsoleApp")
    location("Gui")
    language ("C++")
+   cppdialect ("C++17")
+   staticruntime ("on")
+   
    targetdir ("%{prj.name}/bin/%{cfg.buildcfg}")
    objdir ("%{prj.name}/binobj/%{cfg.buildcfg}")
+   libdirs{"$(SolutionDir)Kengine/bin/%{cfg.buildcfg}"}
+			
+   includedirs { "%{prj.name}/include",
+   "$(SolutionDir)Kengine/include" }
 
-   files { "%{prj.name}/**.h", "%{prj.name}/**.cpp" }
+   files { "%{prj.name}/**.h", "%{prj.name}/**.cpp", 
+			"%{prj.name}/**.ui", "%{prj.name}/**.qrc"}
 
-   filter("configurations:Debug")
+   qt.enable()
+   qtpath ("C:/Qt/5.12.3/" .. compilerversion)
+   qtmodules { "core", "gui", "widgets" }
+   qtprefix ("Qt5")
+   qtgenerateddir("Gui/")
+
+   links{"Kengine"}
+   
+   local cmd = "{COPY} C:/Qt/5.12.3/" .. compilerversion .. "/bin/"
+   
+   filter("configurations:Debug")   
+	
+	  postbuildcommands{ 
+		(cmd .. "Qt5Cored.dll" .. " bin/%{cfg.buildcfg}"),
+		(cmd .. "Qt5Guid.dll" .. " bin/%{cfg.buildcfg}"),
+		(cmd .. "Qt5Widgetsd.dll" .. " bin/%{cfg.buildcfg}")
+	  }
+	  
+	  qtsuffix("d")
       defines { "DEBUG" }
+	  runtime("Debug")
       symbols("On")
 
    filter("configurations:Release")
+      postbuildcommands{ 
+		(cmd .. "Qt5Core.dll" .. " bin/%{cfg.buildcfg}"),
+		(cmd .. "Qt5Gui.dll" .. " bin/%{cfg.buildcfg}"),
+		(cmd .. "Qt5Widgets.dll" .. " bin/%{cfg.buildcfg}")
+	  }
+
       defines { "RELEASE" }
+	  runtime("Release")
       optimize ("On")
 	  
 	filter ("platforms:x64")
 	  architecture ("x64")
 	  
-	includedirs { "%{prj.name}/include" }
-	
 	filter("system:windows")
 		systemversion("latest")
-		
